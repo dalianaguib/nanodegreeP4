381eeb4cef2efc6b9548deb24bbdd3ddabf10b2c
EC2 Default User <ec2-user@ip-172-31-14-51.us-west-2.compute.internal>
2020-05-18 00:13:30 +0000

    adding files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,20 @@
+
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=dalianaguib/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+/home/ec2-user/kubectl run project4 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+/home/ec2-user/kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+/home/ec2-user/kubectl port-forward deployment/project4 8080:80
